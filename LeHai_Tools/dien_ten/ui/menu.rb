76ed40b4093f9ce7040b24edf986166f22ab6917
tankfire4-dot <tankfire4@gmail.com>
2026-06-08 16:46:22 +0700

    Update menu.rb

--- LeHai_Tools/dien_ten/ui/menu.rb
+++ LeHai_Tools/dien_ten/ui/menu.rb
@@ -4,9 +4,9 @@ module TuDong
   module DienTen
     def self.create_cmd
       icons_path = File.join(PATH, 'icons')
-      cmd = UI::Command.new('Ho Tro Dien Ten Nhanh') { TuDong::DienTen.run }
-      cmd.tooltip         = 'Ho tro dien ten nhanh cho component/group duoc chon'
-      cmd.status_bar_text = 'Chon component/group roi chay lenh nay de mo bang dien ten nhanh.'
+      cmd = UI::Command.new('Hỗ Trợ Điền Tên Nhanh') { TuDong::DienTen.run }
+      cmd.tooltip         = 'Hỗ trợ điền tên nhanh cho component/group được chọn'
+      cmd.status_bar_text = 'Chọn component/group rồi chạy lệnh này để mở bảng điền tên nhanh.'
       cmd.small_icon      = File.join(icons_path, 'icon_16.png')
       cmd.large_icon      = File.join(icons_path, 'icon_24.png')
       cmd
